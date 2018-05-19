3d2d785b8aab5d8339c538a2da0dc18ccd8c0398
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2018-05-19 12:57:58 +0300

    improve g= opt

--- ini-parse.js
+++ ini-parse.js
@@ -8,11 +8,14 @@ let props = hash => {
 }
 let se = s => "'" + s.replace(/'/g, "'\\''") + "'"
 let xargs_escape = o => se(':' + JSON.stringify(o))
+let match = (patrn, dest, url) => {
+    return /^url:/.test(patrn) ? url.match(patrn.slice(4)) : dest.match(patrn)
+}
 
 let parser = new ini.IniReader()
 parser.load(process.argv[2])
 Object.entries(parser.getBlock()).forEach( ([k,v]) => {
     if (!v.dest) throw new Error(`'${k}' hash no 'dest' prop!`)
-    if (process.argv[3] && !v.dest.match(process.argv[3])) return // g= opt
+    if (process.argv[3] && !match(process.argv[3], v.dest, k)) return // g= opt
     process.stdout.write(xargs_escape(Object.assign({'.url': k}, ...props(v))) + "\n")
 })
